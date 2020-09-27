74a1f45540d76309ea6b47be6851294f13211800
jesse.lindberg <jesse.lindberg@aalto.fi>
2020-09-27 19:56:51 +0300

    Siistitty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -18,32 +18,7 @@ const App = () => {
         exercises: 14
       }
     ]
-  }  // const part1 = {
-  //   name: 'Fundamentals of React',
-  //   exercises: 10
-  // }
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7
-  // }
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14
-  // }
-  // const parts = [
-  //   {
-  //     name: 'Fundamentals of React',
-  //     exercises: 10
-  //   },
-  //   {
-  //     name: 'Using props to pass data',
-  //     exercises: 7
-  //   },
-  //   {
-  //     name: 'State of a component',
-  //     exercises: 14
-  //   }
-  // ]
+  }
 
   return (
     <div>
