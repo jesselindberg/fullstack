4d4b87972c081de25b13dd75859425d65cfa4bd3
jesse.lindberg <jesse.lindberg@aalto.fi>
2020-09-27 20:16:28 +0300

    1.11* unicafe step6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -43,26 +43,40 @@ const Statistics = (props) => {
   let positive = good / all
   if (all > 0) {
     return (
-      <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
     )
   } else {
     return (
       <p>No feedback given</p>
     )
   }
-}
-
-const StatisticLine = (props) => {
-  return (
-    <p>{props.text} {props.value}</p>
-  )
 }
 
 ReactDOM.render(<App />,
