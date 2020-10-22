7106f18682f351e5125e58cf546c719496c65bd6
jesse.lindberg <jesse.lindberg@aalto.fi>
2020-10-23 02:58:27 +0300

    Siirrä kurssitiedot

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -4,29 +4,41 @@ import ReactDOM from 'react-dom'
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content part1={course.parts[0]} part2={course.parts[1]} part3={course.parts[2]} />
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <Course course={course} />
     </div>
   )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content part1={props.course.parts[0]} part2={props.course.parts[1]} part3={props.course.parts[2]} />
+      <Total total={props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} />
+    </div>
+  ) 
 }
 
 const Header = (props) => {
